7aec4a34ff81052a10483d1b00ebaa93312cb7c3
PhamminhCS82 <pminh723@gmail.com>
2021-09-21 18:20:48 +0700

    new project

---
+++ BTN_LTCSDL/DAO/DAO_SanPham.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTN_LTCSDL.DAO
+{
+    
+    class DAO_SanPham
+    {
+        LaptopPCSaleEntities db;
+        public DAO_SanPham() 
+        {
+            db = new LaptopPCSaleDBEntities();
+        }
+        public dynamic LayDSSanPham()
+        {
+            var ds = db.Products.Select(s => new
+            {
+                s.ProductID,
+                s.ProductName,
+                s.UnitPrice,
+                s.QuantityPerUnit,
+                s.Supplier.CompanyName,
+                s.Category.CategoryName
+            }).ToList();
+            return ds;
+        }
+    }
+}
